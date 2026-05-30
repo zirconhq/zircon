87b69e1342f5b9cf6b7465a0c7858c5ed986240e
Marek Brieger <m.brieger@appcited.com>
2026-05-30 12:54:54 +0000

    feat: set up devcontainer with Docker Compose and add configuration for notes plugin

--- apps/backend/src/bootstrap.ts
+++ apps/backend/src/bootstrap.ts
@@ -1,13 +1,13 @@
 import { readFileSync } from 'node:fs'
-import { cwd } from 'node:process'
 import { resolve } from 'node:path'
+import { homedir } from 'node:os'
 
 import type { ResourceProvider } from '@appcited/james-core'
 
 import { FileProvider } from '#/src/providers/FileProvider.js'
 import { ConfigService } from '#/src/services/ConfigService.js'
 
-const configFilePath = resolve(cwd(), '.james/config.json')
+const configFilePath = resolve(homedir(), '.james/config.json')
 const configFile = JSON.parse(readFileSync(configFilePath, 'utf8'))
 const configService = new ConfigService(configFile)
 
